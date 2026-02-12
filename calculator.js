74a1532c2b0a70a5fc4d8b3c7a48f874b96b2136
Nathan Hoang <nhoang6@lsu.edu>
2026-02-11 23:42:11 -0600

    Created button functionality to JS file

--- calculator.js
+++ calculator.js
@@ -1,28 +1,97 @@
-function operate(str) {
+let onScreen = document.querySelector("#screen");
+
+const clearScreenBtn = document.querySelector("#clear");
+const invertBtn = document.querySelector("#invert");
+const percentBtn = document.querySelector("#percent");
+const divideBtn = document.querySelector("#divide");
+
+const buttons = document.querySelectorAll(".button");
+buttons.forEach(button => {
+	button.addEventListener("click", () => fireButton(button));
+});
+
+function fireButton(button) 
+{
+	console.log(button.id);
+	switch (button.id) 
+	{
+		case "clear":
+			allClear();
+			
+			break;
+
+		case "invert":
+			console.log("Invert!");
+			break;
+
+		case "percent":
+			console.log("Percent!");
+			break;
+
+		case "equals":
+			onScreen.textContent = operate(onScreen.textContent);
+			break;
+	
+		default:
+			if (onScreen.textContent === "0") onScreen.textContent = "";
+			updateScreen(button.textContent)
+			break;
+	};
+}
+
+function updateScreen(value) 
+{
+	switch (value) 
+	{
+		case "÷":
+		case "×":
+		case "–":
+		case "+":
+			onScreen.textContent += " " + value + " "
+			break;
+	
+		default:
+			onScreen.textContent += value
+			break;
+	}
+}
+
+function allClear() 
+{
+	onScreen.textContent = "0";
+}
+
+function operate(str) 
+{
 	const split = str.split(' '),
 		a = +split[0],
 		opr = split[1],
 		b = +split[2];
 
-	const methods = {
+	const methods = 
+	{
 		"+": (a, b) => a + b,
-		"-": (a, b) => a - b,
-		"*": (a, b) => a * b,
-		"/": (a, b) => a / b,
+		"–": (a, b) => a - b,
+		"×": (a, b) => a * b,
+		"÷": (a, b) => a / b,
 	};
 
-	if (!methods[opr] || isNaN(a) || isNaN(b)) {
+	if (!methods[opr] || isNaN(a) || isNaN(b)) 
+	{
 		return NaN;
 	}
 	const answer = methods[opr](a, b);
 	return +(answer + Number.EPSILON).toFixed(5);
 }
 
-function testCalculations() {
-	for (let index = 0; index < 10; index++) {
+function testCalculations() 
+{
+	for (let index = 0; index < 10; index++) 
+		{
 		let randA = Math.floor(Math.random() * 100) + 1;
 		let randB = Math.floor(Math.random() * 100) + 1;
-		let operators = {
+		let operators = 
+		{
 			symbol : ["+", "-", "*", "/"],
 			word : ["sum of", "difference between", "product of", "quotient of"],
 		};
