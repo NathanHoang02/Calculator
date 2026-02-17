36568793ca3ba598de6465bd0175e055c2fdcbb8
Nathan Hoang <nhoang6@lsu.edu>
2026-02-17 13:05:38 -0600

    Fixed Error

--- calculator.js
+++ calculator.js
@@ -1,122 +1,137 @@
 const activeNum = document.querySelector("#active-num");
 const expression = document.querySelector("#expression");
-
 const clearBtn = document.querySelector("#clear");
+const darkMode = document.querySelector(".switch");
 
-let fixedLimit = 10;
-const Operators = ["÷", "×", "–", "+"];
+let fixedLimit = 8;
+
+const operators = ["÷", "×", "–", "+"]
+
 
 const buttons = document.querySelectorAll(".button");
 buttons.forEach(button => 
-	{
-	button.addEventListener("click", () => calculate(button.textContent));
+{
+	button.addEventListener("click", () => 	calculate(button.textContent));
 });
 
-window.addEventListener("keydown", (e)=> 
+window.addEventListener("keydown", (e) => 
 	{
-		if(e.key === "Backspace") 
-			{
-				backspaceActiveNum(); 
-			}
-		else if 
-			(e.key === "Enter") 
-			{
-				calculate("=");
-			}
-		else if 
-			(e.key.length > 1) 
-			{
-				return; 
-			}
-		else if 
-			(parseInt(e.key))
-			{
-				calculate(e.key);
-			}
-		else 
-			{
-				const buttonFromKey = convertKeyToButtonText(e.key)
-				if (buttonFromKey) 
-					{
-						calculate(buttonFromKey); 
-					}
-			}
-	}); 
+	if (e.key === "Backspace") 
+	{
+		backspaceActiveNum();
+	} else if (e.key === "Enter") 
+	{
+		calculate("=");
+	} else if (e.key.length > 1) 
+	{
+		return;	
+	}
+	else if (parseInt(e.key)) 
+	{
+		calculate(e.key); 
+	} else 
+	{
+		const buttonFromKey = convertKeyToButtonText(e.key)
+		if (buttonFromKey) 
+		{ 
+			calculate(buttonFromKey);
+		}
+	}
+});
 
-function backspaceActiveNum() {
+function backspaceActiveNum() 
+{
 	activeNum.textContent = activeNum.textContent.slice(0, -1);
-	if (activeNum.textContent === "") {
+	if (activeNum.textContent === "") 
+	{
 		halfClear();
 	}
 }
 
-function convertKeyToButtonText(eventKey) {
-	switch (eventKey) {
+function convertKeyToButtonText(eventKey) 
+{
+	switch (eventKey) 
+	{
 		case "x":
 		case "*":
 			return "×";
-	
+
 		case "/":
 			return "÷";
-	
+
 		case "-":
 			return "–";
-	
+
 		case "a":
 			return "+";
-			
+
 		case "p":
 			return "%";
-		
+
 		case "c":
 			return "C";
-		
+
 		case "i":
 			return "+/-";
-		
+
 		case "f":
-		case "!":
-			displayFactorial(activeNum.textContent);
+			return "!"
 			break;
 
+		case "!":
+
 		case ".":
+
 		case "%":
+
 		case "–":
+
 		case "÷":
+
 		case "+":
+
 		case "=":
+
+		case "0":
 			return eventKey;
-		
+
 		default:
 			return;
 	}
 }
-function calculate(buttonText)
+
+function calculate(buttonText) 
 {
 	switch (buttonText) 
 	{
 		case "C":
-
 		case "AC":
 			checkHalfClear() ? halfClear() : allClear();
 			break;
 
-		case"+/-": 
-			activeNum.textContent =+(activeNum.textContent * -1).toFixed(fixedLimit); 
-			break; 
+		case "+/-":
+			activeNum.textContent = +(activeNum.textContent * -1).toFixed(fixedLimit);
+			break;
 
-		case "%": 
-			activeNum.textContent =+ (activeNum.textContent / 100).toFixed(fixedLimit); 
-			break; 
+		case "%":
+			activeNum.textContent = +(activeNum.textContent / 100).toFixed(fixedLimit);
+			break;
 
-		case ".": 
-			clearBtn.textContent = "C"; 
-			updateActiveNum(buttonText);
-			break; 
+		case ".":
+			clearBtn.textContent = "C";
+			if (!activeNum.textContent.includes("."))
+			{
+				updateActiveNum(buttonText, "add");
+			}
+			break;
 
-		case "=": 
-		checkRepeatEquals(); 
-		break; 
+		case "!":
+			displayFactorial(activeNum.textContent);
+			break;
+
+		case "=":
+			checkRepeatEquals();
+			break;
 
 		case "÷":
 
@@ -125,55 +140,77 @@ function calculate(buttonText)
 		case "–":
 
 		case "+":
-		fireOperator(buttonText); 
-		break; 
+			fireOperator(buttonText);
+			break;
 
-		default: 
-		if (activeNum.textContent === "0")
-		activeNum.textContent = ""; 
-		clearBtn.textContent = "C"; 
-		updateActiveNum(buttonText); 
-		break; 
-	}; 
+		default:
+			if (activeNum.textContent === "0") activeNum.textContent = "";
+			clearBtn.textContent = "C";
+			updateActiveNum(buttonText, "add");
+			break;
+	};
 }
 
-checkRepeatEquals()
+function checkRepeatEquals() 
 {
-	if(+activeNum.textContent === 0) 
-		return;
-
-	const oprPresent = Operators.some(operator => {
-		return expression.textContent.includes(operator);
+	if (+activeNum.textContent === 0) return;
+	const operatorPres = operators.some(operator => 
+	{
+		return expression.textContent.includes(operator)
 	});
-	const IsEmpty = expression.textContent.split("").includes("");
-	if (IsEmpty && oprPresent)
+	const bIsEmpty = expression.textContent.split(" ").includes("");
+	if (!operatorPres) return;
+	else if (bIsEmpty && operatorPres) 
+	{
 		runEquals(true);
-	else
-		{
-			let expArray = expression.textContent.split(" ");
-			expArray.splice(0, 1, activeNum.textContent);
-			expression.textContent = expArray.join(" ");
-			runEquals(false);
-		}
+	} else 
+	{
+		let expArray = expression.textContent.split(" ");
+		expArray.splice(0, 1, activeNum.textContent);
+		expression.textContent = expArray.join(" ");
+		runEquals(false);
+	}
 }
 
 function runEquals(addToEnd) 
 {
-	if (addToEnd)
-	expression.textContent += activeNum.textContent;
-	activeNum.textContent = operate(expression.textContent);
+	if (addToEnd) expression.textContent += activeNum.textContent;
+	updateActiveNum(operate(expression.textContent), "set");
 }
 
-function updateActiveNum(value)
+function updateActiveNum(value, setAdd) 
 {
-	activeNum.textContent += value;
+	if (value.toString().length > 14 && value < 9999999999) 
+	{
+		value = value.toFixed(fixedLimit);
+	} else if (value.toString().length > 14 && value > 9999999999) 
+	{
+		value = value.toExponential(fixedLimit);
+	}
+	if (setAdd ===  "set") 
+	{
+		activeNum.textContent = value
+	}
+	if (setAdd ===  "add") 
+	{
+		activeNum.textContent += value
+	}
 }
 
 function updateExpression(value) 
 {
+	if (value.toString().length > 14) 
+	{
+		expression.style.fontSize = "10px";
+	}
 	expression.textContent = "";
 	expression.textContent += `${activeNum.textContent} ${value} `;
 	activeNum.textContent = "0";
+}
+
+function checkHalfClear() 
+{
+	return clearBtn.textContent === "C";
 }
 
 function allClear() 
@@ -187,27 +224,6 @@ function halfClear()
 {
 	activeNum.textContent = "0";
 	clearBtn.textContent = "AC";
-}
-
-function checkHalfClear() 
-{
-	return clearBtn.textContent === "C";
-}
-
-function fireOperator(button)
-{
-	const oprPresent = Operators.some(operator => 
-		{
-			return expression.textContent.includes(operator)
-		}); 
-	
-	const isEmpty = expression.textContent.split("").includes(""); 
-	if (isEmpty && oprPresent) 
-		runEquals(); 
-	else 
-		{
-			updateExpression(button.textContent); 
-		}
 }
 
 function operate(str) 
@@ -233,20 +249,32 @@ function operate(str)
 	return +(answer + Number.EPSILON).toFixed(fixedLimit);
 }
 
-// function testCalculations() 
-// {
-// 	for (let index = 0; index < 10; index++) 
-// 		{
-// 		let randA = Math.floor(Math.random() * 100) + 1;
-// 		let randB = Math.floor(Math.random() * 100) + 1;
-// 		let operators = 
-// 		{
-// 			symbol : ["+", "-", "*", "/"],
-// 			word : ["sum of", "difference between", "product of", "quotient of"],
-// 		};
-// 		let chooser = Math.floor(Math.random() * 4);
-	
-// 		console.log(`The ${operators.word[chooser]} ${randA} and ${randB} is ${operate(`${randA} ${operators.symbol[chooser]} ${randB}`)}`);
-// 	};
-// }
-// testCalculations();
+function fireOperator(buttonText) 
+{
+	const operatorPres = operators.some(operator => 
+	{
+		return expression.textContent.includes(operator)
+	});
+	const bIsEmpty = expression.textContent.split(" ").includes("");
+	if (bIsEmpty && operatorPres) 
+	{
+		runEquals(true);
+		updateExpression(buttonText);
+		activeNum.textContent = "";
+	} else 
+	{
+		updateExpression(buttonText);
+	}
+}
+
+function displayFactorial(value) 
+{
+	expression.textContent = `${value}!`
+	updateActiveNum(factorial(value), "set");
+}
+
+function factorial(num) 
+{
+	num = Math.floor(Math.abs(num));
+	return num === 0 ? 1 : num * factorial(num -1);
+};
